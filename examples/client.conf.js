09f8eeb10699190b12ef0165dd0af77602a0a054
Aleh Vasilyeu <aleh_vasilyeu@epam.com>
2019-01-25 15:57:11 +0300

    working solution with logs

--- examples/client.conf.js
+++ examples/client.conf.js
@@ -1,8 +1,8 @@
 module.exports = {
     token: '59b97c34-58e1-4aa9-bc76-78185628adfa',
     endpoint: 'http://52.29.14.221:8080/api/v1',
-    launch: 'LAUNCH_NAME',
-    project: 'PROJECT_NAME',
+    launch: 'My launch',
+    project: '7tvott',
     description: 'Lunch description (optional)',
     tags: [
         'tag1',
