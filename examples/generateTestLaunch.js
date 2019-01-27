027760edbfdacef079b95552bdf330189af877cb
Aleh Vasilyeu <oavasiliev@gmail.com>
2019-01-27 18:00:49 +0300

    simplify example

--- examples/generateTestLaunch.js
+++ examples/generateTestLaunch.js
@@ -24,6 +24,7 @@ const tempStepIds = [];
 
 const startTime = Date.now().valueOf();
 
+// create new launch
 const launchObj = rpClient.startLaunch({
     name: 'Client test',
     start_time: rpClient.helpers.now(),
@@ -31,134 +32,121 @@ const launchObj = rpClient.startLaunch({
     tags: ['your', 'tags'],
 });
 
-// launchObj;
+// update existing launch with new info
+rpClient.updateLaunch(launchObj.tempId, {
+    description: 'test description',
+    tags: ['test', 'tag'],
+});
 
-Promise.resolve()
-    // launchObj.promise
-    // update existing launch with new info
-    .then(() => {
-        rpClient.updateLaunch(launchObj.tempId, {
-            description: 'test description',
-            tags: ['test', 'tag'],
-        });
-    })
-    // add suites to existing launch
-    .then(() => {
-        for (let i = 0; i < 5; i += 1) {
-            const suiteObj = rpClient.startTestItem(
-                {
-                    description: uniqid(),
-                    name: uniqid(),
-                    start_time: rpClient.helpers.now(),
-                    type: 'SUITE',
-                },
-                launchObj.tempId,
-            );
-            tempSuiteIds.push(suiteObj.tempId);
-        }
-    })
-    // add nested suites
-    .then(() => {
-        tempSuiteIds.forEach((tempSuiteId) => {
-            for (let i = 0; i < 2; i += 1) {
-                const nestedSuiteObj = rpClient.startTestItem(
-                    {
-                        description: uniqid(),
-                        name: `NESTED ${uniqid()}`,
-                        start_time: rpClient.helpers.now(),
-                        type: 'SUITE',
-                    },
-                    launchObj.tempId,
-                    tempSuiteId,
-                );
-                nestedTempSuiteIds.push(nestedSuiteObj.tempId);
-            }
-        });
-    })
-    // add steps to suites
-    .then(() => {
-        nestedTempSuiteIds.forEach((nestedTempSuiteId) => {
-            for (let i = 0; i < 2; i += 1) {
-                const stepObj = rpClient.startTestItem(
-                    {
-                        description: uniqid(),
-                        name: uniqid(),
-                        start_time: rpClient.helpers.now(),
-                        type: 'STEP',
-                    },
-                    launchObj.tempId,
-                    nestedTempSuiteId,
-                );
-                tempStepIds.push(stepObj.tempId);
-            }
-        });
-    })
-
-    // add logs and attachments to the steps
-    .then(() => {
-        tempStepIds.forEach((tempStepId) => {
-            for (let i = 0; i < 2; i += 1) {
-                rpClient.sendLog(
-                    tempStepId,
-                    {
-                        level: 'INFO',
-                        message: uniqid(),
-                        time: rpClient.helpers.now(),
-                    },
-                );
-                rpClient.sendLogWithFile(
-                    tempStepId,
-                    {
-                        level: 'INFO',
-                        message: uniqid(),
-                        time: rpClient.helpers.now(),
-                    },
-                    {
-                        name: uniqid(),
-                        type: 'image/png',
-                        content: screenshot,
-                    },
-                );
-            }
-        });
-    })
-    // mark as failed all steps
-    .then(() => {
-        tempStepIds.map(tempStepId =>
-            rpClient.finishTestItem(
-                tempStepId,
-                {
-                    end_time: rpClient.helpers.now(),
-                    status: 'failed',
-                },
-            ).promise);
-    })
-    // mark as passed all suites, only in that order
-    .then(() => {
-        nestedTempSuiteIds.map(nestedTempSuiteId => rpClient.finishTestItem(
-            nestedTempSuiteId,
-            {
-                end_time: rpClient.helpers.now(),
-                status: 'passed',
-            },
-        ).promise);
-    })
-    .then(() => {
-        tempSuiteIds.map(tempSuiteId => rpClient.finishTestItem(
-            tempSuiteId,
-            {
-                end_time: rpClient.helpers.now(),
-                status: 'passed',
-            },
-        ).promise);
-    })
-    // finish launch
-    .then(() => rpClient.finishLaunch(
+// add suites to existing launch
+for (let i = 0; i < 5; i += 1) {
+    const suiteObj = rpClient.startTestItem(
+        {
+            description: uniqid(),
+            name: uniqid(),
+            start_time: rpClient.helpers.now(),
+            type: 'SUITE',
+        },
         launchObj.tempId,
+    );
+    tempSuiteIds.push(suiteObj.tempId);
+}
+
+// add nested suites
+tempSuiteIds.forEach((tempSuiteId) => {
+    for (let i = 0; i < 2; i += 1) {
+        const nestedSuiteObj = rpClient.startTestItem(
+            {
+                description: uniqid(),
+                name: `NESTED ${uniqid()}`,
+                start_time: rpClient.helpers.now(),
+                type: 'SUITE',
+            },
+            launchObj.tempId,
+            tempSuiteId,
+        );
+        nestedTempSuiteIds.push(nestedSuiteObj.tempId);
+    }
+});
+
+// add steps to suites
+nestedTempSuiteIds.forEach((nestedTempSuiteId) => {
+    for (let i = 0; i < 2; i += 1) {
+        const stepObj = rpClient.startTestItem(
+            {
+                description: uniqid(),
+                name: uniqid(),
+                start_time: rpClient.helpers.now(),
+                type: 'STEP',
+            },
+            launchObj.tempId,
+            nestedTempSuiteId,
+        );
+        tempStepIds.push(stepObj.tempId);
+    }
+});
+
+// add logs and attachments to the steps
+tempStepIds.forEach((tempStepId) => {
+    for (let i = 0; i < 2; i += 1) {
+        rpClient.sendLog(
+            tempStepId,
+            {
+                level: 'INFO',
+                message: uniqid(),
+                time: rpClient.helpers.now(),
+            },
+        );
+        rpClient.sendLogWithFile(
+            tempStepId,
+            {
+                level: 'INFO',
+                message: uniqid(),
+                time: rpClient.helpers.now(),
+            },
+            {
+                name: uniqid(),
+                type: 'image/png',
+                content: screenshot,
+            },
+        );
+    }
+});
+
+// mark as failed all steps
+tempStepIds.forEach(tempStepId =>
+    rpClient.finishTestItem(
+        tempStepId,
         {
             end_time: rpClient.helpers.now(),
+            status: 'failed',
         },
-    ).promise.then(() => {
-        // eslint-disable-next-line no-console
-        console.log('Execution took: ', Date.now().valueOf() - startTime);
-    }));
+    ));
+
+// mark as passed all suites, only in that order
+nestedTempSuiteIds.forEach(nestedTempSuiteId => rpClient.finishTestItem(
+    nestedTempSuiteId,
+    {
+        end_time: rpClient.helpers.now(),
+        status: 'passed',
+    },
+));
+
+tempSuiteIds.forEach(tempSuiteId => rpClient.finishTestItem(
+    tempSuiteId,
+    {
+        end_time: rpClient.helpers.now(),
+        status: 'passed',
+    },
+));
+
+// finish launch
+rpClient.finishLaunch(
+    launchObj.tempId,
+    {
+        end_time: rpClient.helpers.now(),
+    },
+).promise.then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Execution took: ', Date.now().valueOf() - startTime);
+});
