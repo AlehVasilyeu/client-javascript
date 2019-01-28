72340deed17b56e8209b46e7c89dbcd5746d5de7
Aleh Vasilyeu <oavasiliev@gmail.com>
2019-01-28 23:13:57 +0300

    fix test

--- spec/report-portal-client.spec.js
+++ spec/report-portal-client.spec.js
@@ -379,7 +379,7 @@ describe('ReportPortal javascript client', () => {
         it('rejects promise with error if launch was not found by tempId', (done) => {
             client.updateLaunch(UNEXISTENT_TEMP_LAUNCH_ID).promise.catch((error) => {
                 expect(error instanceof Error).toBeTruthy();
-                expect(error.message).toMatch(`Launch "${UNEXISTENT_TEMP_LAUNCH_ID}" not found`);
+                expect(error.message).toMatch(`Item "${UNEXISTENT_TEMP_LAUNCH_ID}" not found`);
                 done();
             });
         });
